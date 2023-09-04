d486dad564df509d79831ade48cc4da3620a84f7
YMRLK <moryliak.y@gmail.com>
2023-09-04 14:32:26 -0700

    Start to consume first steps - creating and nesting components - styling components -writing JSX markup

---
+++ src/components/about-page/AboutPage.tsx
@@ -0,0 +1,10 @@
+import './AboutPage.css';
+
+export default function AboutPage() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Hello there. <br />How are youtr doing?</p>
+        </>
+    );
+}
