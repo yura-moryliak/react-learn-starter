926620ccdaec64aad8e7e7cf908203976275e00a
YMRLK <moryliak.y@gmail.com>
2023-09-15 19:12:45 -0700

    State and state upligting

--- src/components/my-button/MyButton.tsx
+++ src/components/my-button/MyButton.tsx
@@ -2,19 +2,12 @@ import { useState } from 'react';
 
 import './MyButton.css';
 
-export default function MyButton() {
-
-    const [ count, setCount ] = useState(0);
-
-    function handleClick(): void {
-        setCount(count + 1);
-    }
+// @ts-ignore
+export default function MyButton({ count, onClick }) {
 
     return (
-        <div
-            className="my-button-wrapper"
-            onClick={handleClick}>
-            <button>I'm a button, Click me { count }</button>
+        <div className="my-button-wrapper">
+            <button onClick={onClick}>I'm a button, Click me { count }</button>
         </div>
     );
 }
